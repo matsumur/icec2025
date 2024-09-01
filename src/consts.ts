b8cf823d0d320237d6c0ab5313c36601ba9d01fc
Kohei Matsumura <matsumur@acm.org>
2024-09-01 10:45:33 +0900

    minor fix

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'IFIP ICEC2025';
+export const SITE_TITLE = 'IFIP/IPSJ ICEC2025';
 export const SITE_DESCRIPTION = 'The 24th IFIP International Conference on Entertainment Computing';
